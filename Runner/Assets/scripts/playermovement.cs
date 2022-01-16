ba7fec170b1e96ba692b99fd488e6304d2ab1e49
Brandon <brandon101morgan@outlook.com>
2022-01-16 15:54:19 -0600

    Update UI amd added code for movement

--- Runner/Assets/scripts/playermovement.cs
+++ Runner/Assets/scripts/playermovement.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
 
-    public float moveSpeed = 4f;
+    public float moveSpeed = 15f;
+    public float mousesenX = 1f;
+    public float mousesenY = 1f;
+    float verticalLockRotation;
+    Transform mainCam;
 
+
+    //hides mouse cursor in game window press esc key to unlock cursor 
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        mainCam = Camera.main.transform;
+    }
+        
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +29,15 @@ public class playermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //input from player
+       //transform.position = mainCam.transform.forward * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mousesenX);
+        verticalLockRotation += Input.GetAxis("Mouse Y") * mousesenY;
+        //verticaLockRotation = Mathf.Clamp(verticalLockRotation, -80 80);
+        mainCam.localEulerAngles = Vector3.left * verticalLockRotation;
+
+
+        //controls and movement for the 4 main keys
         Vector3 posit = transform.position;
 
         if (Input.GetKey("w"))
@@ -35,7 +56,6 @@ public class playermovement : MonoBehaviour
         {
             posit.x -= moveSpeed * Time.deltaTime;
         }
-
 
         transform.position = posit;
     }
