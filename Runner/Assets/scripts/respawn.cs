bdca7dfa6b787e2f268843240c0ba7d2ea06f785
Brandon <brandon101morgan@outlook.com>
2022-01-16 00:04:58 -0600

    If player falls respawn

---
+++ Runner/Assets/scripts/respawn.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections; 
+
+public class respawn : MonoBehaviour 
+{
+    public float threshold;
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < threshold)
+            transform.position = new Vector3(0, 0, 0);
+    }
+}
